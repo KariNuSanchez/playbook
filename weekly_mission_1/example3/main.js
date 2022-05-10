581c757c375a8823a4c749e7654db7e8a583709e
Karina Nuñez <99064554+KariNuSanchez@users.noreply.github.com>
2022-05-10 18:47:36 -0500

    Add files via upload

---
+++ weekly_mission_1/example3/main.js
@@ -0,0 +1,9 @@
+/*
+  node main.js
+*/
+
+const logger1 = require('./logger1')
+const logger2 = require('./logger2')
+
+logger1('This is an informational message')
+logger2.verbose('This is a verbose message')
---
+++ weekly_mission_1/example7/main.js
@@ -0,0 +1,4 @@
+// Example 7: EcmaScript Modules ESM
+import * as loggerModule from './logger.js'
+
+console.log(loggerModule)
